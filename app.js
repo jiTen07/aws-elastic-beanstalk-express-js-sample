d540b0f902967f17ad6a86d6eac749a09bbdc36b
JT <jiten_07@yahoo.co.in>
2024-02-24 22:16:10 +0530

    updated app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello, Its great to see you in cloud journey !'));
+app.get('/', (req, res) => res.send('Hello, its great to see you in cloud journey !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
